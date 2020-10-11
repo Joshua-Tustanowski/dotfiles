68f2b1a61d9152d141477efaf32c7ba3bed8a115
Joshua Tustanowski <josh@Joshuas-MacBook-Pro.local>
2020-10-11 01:45:31 +0100

    Added brew.sh, install.sh and vs-code settings

---
+++ brew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install vs-code
+brew cask install docker
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install slack
+brew cask install google-backup-and-sync
+brew cask install gimp
+brew cask install hyperdock
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
